0836b84ca61860dfff10df4ce2879c1663560ce3
UjjwalS2 <145217850+UjjwalS2@users.noreply.github.com>
2026-08-26 17:25:23 +0530

    Add Gemini chat models

---
+++ chat_models/models.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from schema.output_schema import SummarizerOutput
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    temperature=0.5,
+    max_tokens=None,
+)
+
+Summarizer_model = llm.with_structured_output(SummarizerOutput)
+quiz_model = llm
+chat_model = llm
